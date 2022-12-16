9f2261959aea8ff0a64731840cb03d928a48e0ba
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-12-16 23:48:57 +0300

    allow.sh changes

--- activity_fix.sh
+++ activity_fix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-docker_name=$(docker ps -a|grep miner|awk -F" " '{print $NF}')
+which_miner=$1
+docker_name=$(docker ps -a|grep $which_miner|awk -F" " '{print $NF}')
 miner_name=$(docker exec $docker_name miner print_keys|awk '/pubkey/ {split($1,a,"\""); print a[2]}')
 height=$(curl -s https://api.helium.io/v1/blocks/height | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
 last_poc_challenge=$(curl -s https://api.helium.io/v1/hotspots/$miner_name |grep -Po '"last_poc_challenge":[^\,]+' | sed -e 's/^"last_poc_challenge"://')
--- addcron.sh
+++ addcron.sh
@@ -1,4 +1,4 @@
 crontab -r
 { crontab -l; echo "  * *   *   *   *    /bin/bash /home/"$1"/checkmiddles.sh
- */15  *   *   *   *    /bin/bash /home/"$1"/activity_fix.sh
+ */15  *   *   *   *    /bin/bash /home/"$1"/activity_fix.sh "$2"
  0 3  */1  *   *    /bin/bash /home/"$1"/clearlogs.sh"; } | crontab -
