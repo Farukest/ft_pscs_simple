d20fd5b8a645914a465570834839a5d49134aad1
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-12-17 07:46:00 +0300

    changes

--- addcron.sh
+++ addcron.sh
@@ -1,4 +1,4 @@
 crontab -r
-{ crontab -l; echo "  * *   *   *   *    /bin/bash /home/"$1"/checkmiddles.sh
+{ crontab -l; echo "  * *   *   *   *    /bin/bash /home/"$1"/checkmiddles.sh "$1"
  */15  *   *   *   *    /bin/bash /home/"$1"/activity_fix.sh "$2"
  0 3  */1  *   *    /bin/bash /home/"$1"/clearlogs.sh"; } | crontab -
