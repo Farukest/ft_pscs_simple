accc26041215cda9a2ca698c0074ec35e76aeaba
farukest <abdullahfarukozden@gmail.com>
2023-02-02 22:12:58 +0300

    cron to 50min for restart

--- addcron.sh
+++ addcron.sh
@@ -1,5 +1,5 @@
 (crontab -l; echo "  * *   *   *   *    /bin/bash /home/"$1"/checkmiddles.sh "$1"
- *  */3  *  *  *    /bin/bash /home/"$1"/activity_fix.sh "$2"
+ *  */50  *  *  *    /bin/bash /home/"$1"/activity_fix.sh "$2"
  0 3  */1  *   *    /bin/bash /home/"$1"/clearlogs.sh
  
  "; ) | crontab -
